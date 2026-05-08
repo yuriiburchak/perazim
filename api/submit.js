d493811c181f5ea121d46a15089c092497bde00a
Yurii Burchak <_email__github@gmail.com>
2026-05-09 00:08:14 +0300

    Update Google Sheets URL

--- api/submit.js
+++ api/submit.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const TG_TOKEN = process.env.TG_TOKEN || '8712326195:AAEd8vAopTAf2_6xvynfrx1JzBDtMemWk-E';
   const CHAT_ID = process.env.CHAT_ID || '380071501';
-  const GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbww-5v5B7duMO9wFmT1NJDWE0noC0rpcZORNXGoF7eI-D_F93IEqpjsEs0ZDP9N6etS/exec';
+  const GOOGLE_URL = 'https://script.google.com/macros/s/AKfycbzn4LB2DBfSp5SJ62f-xhUgRfWvt_foAG_eAGrwCuRxuLZ2Zi4Kzc74IOS_vnYwBEtA/exec';
 
   try {
     // 1. Send to Telegram
